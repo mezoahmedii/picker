e7574a19fb0609c0d22fb0827cab5f232e3b25e5
mezoahmedii <mezoahmedamin@gmail.com>
2024-10-09 22:03:32 -0400

    store file name and path for future use

--- src/window.py
+++ src/window.py
@@ -9,11 +9,13 @@ class PickerWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'PickerWindow'
 
     toast_overlay = Gtk.Template.Child()
-
     elementsList = Gtk.Template.Child()
     entryRow = Adw.EntryRow(title=_("Add something…"), show_apply_button=True)
 
     latest_removed_item = None
+    currentFile = "~/Documents/Untitled.txt"
+    currentFileTitle = "Untitled File"
+    currentFileContent = ""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -91,6 +93,8 @@ class PickerWindow(Adw.ApplicationWindow):
     def addElement(self, element):
         self.elementsList.add(element)
 
+        self.checkFileContent()
+
     def removeElement(self, widget, element):
         self.latest_removed_item = element
         self.elementsList.remove(element)
@@ -100,6 +104,8 @@ class PickerWindow(Adw.ApplicationWindow):
                 button_label=_("Undo"),
                 action_name="win.restore-element")
         )
+
+        self.checkFileContent()
 
     def onDialogResponse(self, dialog, task, element):
         response = dialog.choose_finish(task)
@@ -126,12 +132,23 @@ class PickerWindow(Adw.ApplicationWindow):
             self.toast_overlay.add_toast("The file isn't encoded properly")
             return
 
+        info = file.query_info("standard::display-name", Gio.FileQueryInfoFlags.NONE)
+        if info:
+            display_name = info.get_attribute_string("standard::display-name")
+        else:
+            display_name = file.get_basename()
+        self.set_title(display_name)
+
+        self.currentFile = file.peek_path()
+        self.currentFileContent = text
+        self.currentFileTitle = display_name
+
         child = self.entryRow.get_parent().get_last_child().get_prev_sibling()
         while child is not None:
             self.elementsList.remove(child.get_next_sibling())
             child = child.get_prev_sibling()
 
-        for element in contents[1].decode("utf-8").splitlines():
+        for element in text.splitlines():
             actionRow = Adw.ActionRow(title=element.replace("&", "&amp;"))
 
             removeButton = Gtk.Button(icon_name="remove-symbolic", valign="center")
@@ -141,6 +158,23 @@ class PickerWindow(Adw.ApplicationWindow):
             actionRow.add_suffix(removeButton)
 
             self.addElement(actionRow)
+
+    def checkFileContent(self):
+        elements = []
+        child = self.entryRow.get_parent().get_first_child().get_next_sibling()
+        while child is not None:
+            elements.append(child.get_title().replace("&amp;", "&"))
+            child = child.get_next_sibling()
+
+        print("a")
+        print(self.currentFileContent)
+        print("y")
+        print("\n".join(elements))
+
+        if "\n".join(elements) == self.currentFileContent:
+            self.set_title(self.currentFileTitle)
+        else:
+            self.set_title(f"• {self.currentFileTitle}")
 
     def createAction(self, name, callback):
         action = Gio.SimpleAction.new(name, None)
